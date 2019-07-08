89dd34eb25e42286dca2a42dbd819401bcbf6707
Harnas_20 <harnas_20@o2.pl>
2019-07-08 17:11:59 +0200

    lesson facebook 2

--- JS/JS2/exercise.js
+++ JS/JS2/exercise.js
@@ -1,6 +1,13 @@
 var database = [
 { username: "Harnas",
-  password:"tescik"}
+  password: "tescik"
+},
+{ username: "Janek",
+  password: "12345"
+},
+{ username: "Marian",
+  password: "333"
+}
 ];
 
 
@@ -10,17 +17,30 @@ var newsfeed = [
 {username: "Sally", timeline: "hi everyone"}
   ];
 
-var userNamePrompt = prompt("Podaj login");
-var passwordPrompt = prompt("Podaj hasło");
+var userNamePrompt = prompt("Podaj login:");
+var passwordPrompt = prompt("Podaj hasło:");
+
+function isUserValid(username, password){
+  for (var i=0; i< database.length; i++) {
+    if(username === database[i].username && password === database[i].password) {
+    return true;
+    }
+  }
+  return false;
+}
 
 function signIn(user, pass) {
-  if(user === database[0].username && pass === database[0].password){
-    console.log(newsfeed);
-  }
-  else {
-    alert("Przepraszam, podałeś błedny Login lub hasło");
 
-  }
+var check = isUserValid(user,pass);
+
+
+  if(check === true){
+     console.log(newsfeed);
+   }
+   else {
+     alert("Przepraszam, podałeś błedny Login lub hasło");
+
+   }
 }
 
 signIn(userNamePrompt,passwordPrompt);
