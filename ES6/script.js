54177779b9bdfb1d6d1986fbffff383f314e9968
Harnas_20 <harnas_20@o2.pl>
2019-07-30 18:38:01 +0200

    New arrow functions and array - map, filter, reduce

--- ES6/script.js
+++ ES6/script.js
@@ -1,3 +1,5 @@
+
+//let i const zamiast var
 let x = 7;
 const z = "Billy";
 
@@ -20,7 +22,7 @@ obj.imie = "stefan";
 console.log(obj.imie);
 
 const name = "Spiderman";
-
+//nowa deklaracja obiektów;
 let obj2 = {
   [name]: "Peter Parker",
   [2*15]: "Green Goblin"
@@ -31,12 +33,44 @@ console.log(obj2);
 const nm= "Kasia";
 const wk= 32;
 
+// nowa możliwość wypisywania stringa
 const text = `Ta dziewczyna ma na imie ${nm} i ma ${wk} lata`;
 console.log(text)
 
 let a=1;
 let b=2;
 
+// arrow functions
 const add = (a,b)=> a+b ;
 
 console.log(add(2,4));
+
+const multiply = (a)=>(b)=> a*b;
+console.log(multiply(3)(3));
+
+const compose = (f,g,v) => (a) => f(g(v(a)));
+const sum = (a) => a+1;
+
+const result = compose(sum,sum,sum)(1);
+
+console.log(result);
+
+const array = [1,2,3,4];
+
+// kopiowanie elementów tablicy razy dwa  map
+const doubleArray = array.map((num) => {
+  return num*2;});
+
+console.log(doubleArray);
+
+//filter - wybranie elementów tablicy większych od 2 - wersja skrócona bez return
+
+const filterArray = array.filter(num => num > 2);
+
+console.log(filterArray);
+
+// reduce
+
+
+const reduceArray = array.reduce((acc,num) => {return acc+num},2);
+console.log(reduceArray);
