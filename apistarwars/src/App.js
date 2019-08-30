920a3f593550ed550fff1375f8dbf0fb3e97dbaf
Harnas_20 <Harnas_20@o2.pl>
2019-08-30 15:03:18 +0200

    small change

--- apistarwars/src/App.js
+++ apistarwars/src/App.js
@@ -3,13 +3,15 @@ class App extends Component {
 
     constructor() {
       super();
-      this.state = { data: [] };
+      this.state = { data: [],
+      x:'' };
     }
     async componentDidMount() {
       const response = await fetch(`https://swapi.co/api/people`);
       const json = await response.json();
       console.log(json.results);
       this.setState({ data: json.results });
+      this.setState({ x: 'działa'});
     }
     render() {
       return (
@@ -17,7 +19,7 @@ class App extends Component {
           <ul>
             {this.state.data.map(el => (
               <li>
-                {el.name}: {el.height}
+                {el.name}: {el.height} : {this.state.x}
               </li>
             ))}
           </ul>
