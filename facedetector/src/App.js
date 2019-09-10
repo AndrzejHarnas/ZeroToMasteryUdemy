6da23f300dd133982f1c33173312d160a79ef1d4
Harnas_20 <Harnas_20@o2.pl>
2019-09-10 12:52:49 +0200

    bounding-box from API working

--- facedetector/src/App.js
+++ facedetector/src/App.js
@@ -35,13 +35,36 @@ class App extends Component {
     super();
     this.state={
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {}
+
     }
   }
 
 onInputChange=(event) => {
   this.setState({input: event.target.value});
 }
+
+
+claculateFaceLocation = (data) => {
+  const clarifaiface = data.outputs[0].data.regions[0].region_info.bounding_box;
+  const image = document.getElementById('inputimage');
+  const width = Number(image.width);
+  const height = Number(image.height);
+  return {
+    leftCol: clarifaiface.left_col * width,
+    topRow: clarifaiface.top_row * height,
+    rightCol: width - (clarifaiface.right_col * width),
+    bottomRow: height - (clarifaiface.bottom_row * height)
+  }
+}
+
+displayFaceBox = (box) => {
+  console.log(box);
+  this.setState({box: box});
+}
+
+
 
 onButtonSubmit = () => {
   this.setState({imageUrl: this.state.input});
@@ -50,14 +73,8 @@ onButtonSubmit = () => {
   app.models.predict(
     Clarifai.FACE_DETECT_MODEL,
     this.state.input)
-    .then(
-    function(response) {
-      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-    },
-    function(err) {
-      // there was an error
-    }
-  );
+    .then(response => this.displayFaceBox(this.claculateFaceLocation(response)))
+    .catch(err => console.log(err));
 }
 
 
@@ -74,6 +91,7 @@ render() {
         onInputChange={this.onInputChange}
         onButtonSubmit={this.onButtonSubmit}/>
         <FaceRecognition
+        box={this.state.box}
         imageUrl={this.state.imageUrl}/>
 
     </div>
