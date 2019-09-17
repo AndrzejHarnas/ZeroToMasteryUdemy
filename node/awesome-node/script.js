4dd3c96724d2fd55201d4daace221d2f73677c18
Harnas_20 <Harnas_20@o2.pl>
2019-09-17 13:44:48 +0200

    File system fs node :)

---
+++ node/awesome-node/script.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+
+fs.readFile('./hello.txt', (err, data) => {
+  if(err) {
+    console.log('errod');
+  }
+  console.log('Async', data.toString('utf8'));
+})
+
+const file = fs.readFileSync('./hello.txt');
+
+console.log('Sync', file.toString());
+
+//Append
+//fs.appendFile('./hello.txt',' This is so cool!', err =>{
+//  if(err) {
+//    console.log(err)
+//  }
+
+//})
+
+//Write
+
+//fs.writeFile('bye.txt', 'Sad to see you go', err=> {
+//  if(err) {
+//    cosole.log(err);
+//  }
+//})
+
+//Delete
+
+fs.unlink('./bye.txt', err => {
+  if(err) {
+    console.log(err);
+  }
+  console.log('inception');
+})
