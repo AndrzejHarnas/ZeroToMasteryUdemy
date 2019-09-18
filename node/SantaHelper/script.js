255442876e1c23d369082c0241c15d4c4e345e6e
Harnas_20 <Harnas_20@o2.pl>
2019-09-18 13:34:34 +0200

    SantaHelper Qest2

--- node/SantaHelper/script.js
+++ node/SantaHelper/script.js
@@ -2,9 +2,12 @@ const fs = require('fs');
 const file = fs.readFileSync('./floors.txt');
 
 console.log(file.toString());
+
+console.time('santa-time');
 const string = file.toString();
 const length = string.length;
 let floor = 0;
+let firstBasement = 0;
 
 
 for(i=0;i<=length-1;i++) {
@@ -12,6 +15,13 @@ for(i=0;i<=length-1;i++) {
   if(string.charAt(i)==='(') { floor += 1; }
   if(string.charAt(i)===')') { floor -= 1; }
 
+  if(floor < 0 && firstBasement === 0) {
+    firstBasement = i;
+  }
+
 }
 
-console.log(floor);
+console.log('Mikołaj doszedł na:', floor, 'piętro');
+console.log('Perwszy raz zszedł do piwnicy po:', firstBasement, 'ruchach' );
+
+console.timeEnd('santa-time');
