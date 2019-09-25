e71050affcda4798dba15733e30b12e059b6e1a0
Harnas_20 <Harnas_20@o2.pl>
2019-09-25 12:49:05 +0200

    /prifle and image

--- facedetector-api/server.js
+++ facedetector-api/server.js
@@ -58,16 +58,36 @@ res.json(database.users[database.users.length-1])
 })
 
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if(user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  })
+  if(!found) {
+    res.status(400).json('not found');
+  }
 })
 
 
-/*
-/ -->res = this is working
-/signin --> POST = success/fail
-/register --> POST = user
-/profile/:userid --> GET = userid
-/image --> PUT --> user
+app.post('/image', (req,res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach(user => {
+    if(user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+    if(!found) {
+      res.status(400).json('not found');
+    }
+  })
+})
 
-*/
+app.listen(3000, ()=> {
+  console.log('app is running on port 3000');
+})
