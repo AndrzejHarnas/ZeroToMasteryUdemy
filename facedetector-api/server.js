de86032ae44fc3493702d874cb7ae845fd5112c7
Harnas_20 <Harnas_20@o2.pl>
2019-11-22 14:11:29 +0100

    del old database objects

--- facedetector-api/server.js
+++ facedetector-api/server.js
@@ -24,35 +24,6 @@ db.select('*').from('users').then(data => {
 app.use(bodyParser.json());
 app.use(cors());
 
-
-const database = {
-  users: [
-    {
-      id: '123',
-      name: 'John',
-      email: 'john@gmail.com',
-      password: 'cookies',
-      entries: 0,
-      joined: new Date()
-    },
-    {
-      id: '124',
-      name: 'Sally',
-      email: 'sally@gmail.com',
-      password: 'cookies',
-      entries: 0,
-      joined: new Date()
-    }
-  ],
-  login: [
-    {
-      id:'987',
-      has:'',
-      email:'john@gmial.com'
-    }
-  ]
-}
-
 app.get('/', (req, res) => {
   res.send(database.users);
 })
